127633bc48789b5c481146cbf9a1d112ce4866a3
Lina Budkova <linabudkova35@gmail.com>
2022-12-23 13:25:43 +0200

    йцу

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -21,33 +21,57 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+      refs.startBtn.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;
+
+      refs.startBtn.addEventListener('click', countdownTime);
+
+      function countdownTime() {
+        timer = setInterval(() => {
+          refs.startBtn.disabled = true;
+
+          const inputMS = new Date(refs.input.value).getTime();
+          const now = new Date().getTime();
+          const timeLeft = inputMS - now;
+
+          const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+          refs.dataDays.innerHTML = days < 10 ? addLeadingZero(days) : days;
+          refs.dataHours.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
+          refs.dataMinutes.innerHTML = minutes < 10 ? addLeadingZero(minutes) : minutes;
+          refs.dataSeconds.innerHTML = seconds < 10 ? addLeadingZero(seconds) : seconds;
+
+          if (timeLeft < 1000) {
+            clearInterval(timer);
+            startBtn.disabled = false;
+          }
+        }, 1000);
+      }
+
+
+      function addLeadingZero(value) {
+        const stringValue = String(value);
+        return stringValue.padStart(2, '0');
+      }
+      
+      function convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+        return { days, hours, minutes, seconds };
+      }
+    }
   },
 };
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-
-
-
-
-
-
-flatpickr(input, options);
+flatpickr(refs.input, options);
