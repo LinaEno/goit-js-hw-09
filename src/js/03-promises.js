fec0ca21f79a2a3a1527a79ed7a4db4303b4093b
Lina Budkova <linabudkova35@gmail.com>
2022-12-23 16:12:40 +0200

    qweqew

---
+++ src/js/03-promises.js
@@ -0,0 +1,52 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+  firstDelay: document.querySelector('[name="delay"]'),
+  delayStep: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+
+}
+
+refs.form.addEventListener('submit', submitCreatePromises);
+
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+function submitCreatePromises(event) {
+
+  event.preventDefault();
+
+  let delay = refs.firstDelay.valueAsNumber;
+  const delayStepNumber = refs.delayStep.valueAsNumber;
+  const amountNumber = refs.amount.valueAsNumber;
+
+  for (let i = 1; i <= amountNumber; i+=1) {
+    createPromise(i, delay)
+      .then(({ i, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${i} in ${delay}ms`
+        );
+      })
+      .catch(({ i, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${i} in ${delay}ms`
+        );
+      });
+    delay += delayStepNumber;
+  }
+}
